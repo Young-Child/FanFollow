d3b047a488ab2b8364f8e6e98b49661fccf2d460
미니 <cow970814@naver.com>
2023-06-29 18:02:01 +0900

    feature : 프로젝트 시작

---
+++ FanFollow/FanFollow/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  FanFollow
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+        
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
