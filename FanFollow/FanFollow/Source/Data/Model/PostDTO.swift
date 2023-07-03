310a1c49da240d1ebe435e842d4092a1e97c2453
KyoPak <hyosing92@gmail.com>
2023-07-03 23:05:13 +0900

    refactor : PostDTO에 createdData 프로퍼티 추가

--- FanFollow/FanFollow/Source/Data/Model/PostDTO.swift
+++ FanFollow/FanFollow/Source/Data/Model/PostDTO.swift
@@ -10,25 +10,28 @@ import Foundation
 struct PostDTO: Decodable {
     let postID: String?
     let userID: String
+    var createdData: String = Date().description
     
     let title: String
     let description: String
     
-    let imageURLs: [String]
-    let videoURL: String
+    let imageURLs: [String]?
+    let videoURL: String?
     
     enum CodingKeys: String, CodingKey {
         case postID = "postId"
+        case createdData = "creadtedAt"
         case userID, title, description, imageURLs, videoURL
     }
     
     func convertBody() -> [String: Any] {
-        return ["postId" : postID ?? UUID().uuidString,
-                "userID" : userID,
-                "title" : title,
-                "description" : description,
-                "imageURLs" : imageURLs,
-                "videoURL" : videoURL
+        return [
+            "postId" : postID ?? UUID().uuidString,
+            "userID" : userID,
+            "title" : title,
+            "description" : description,
+            "imageURLs" : imageURLs as Any,
+            "videoURL" : videoURL as Any
         ]
     }
 }
