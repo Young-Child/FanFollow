03bb43d2ed5c61b597c9879330cde1745de1d158
KyoPak <hyosing92@gmail.com>
2023-07-03 15:56:29 +0900

    feat : Bundle Extension 구현 및 SupabaseInfo.plist  추가하여 Key값 보관

---
+++ FanFollow/FanFollow/Source/Util/Extension/Bundle+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  Bundle+Extension.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/07/03.
+//
+
+import Foundation
+
+extension Bundle {
+    var apiKey: String {
+        guard let file = self.path(forResource: "SupabaseInfo", ofType: "plist") else { return "" }
+        
+        guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
+        
+        guard let key = resource["API_KEY"] as? String else {
+            fatalError("SupabaseInfo.plist에 API_KEY 설정을 해주세요.")
+        }
+        
+        return key
+    }
+}
