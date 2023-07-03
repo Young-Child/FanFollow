c57b19923a9bd65bb6775fc50f040a01d5583d63
KyoPak <hyosing92@gmail.com>
2023-07-03 21:31:19 +0900

    feat : PostRequestDirector 구현

---
+++ FanFollow/FanFollow/Source/NetworkManager/Builder/PostRequestDirector.swift
@@ -0,0 +1,42 @@
+//
+//  PostRequestDirector.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/07/03.
+//
+
+import Foundation
+
+struct PostRequestDirector {
+    let builder: URLRequestBuilder
+    
+    init(builder: URLRequestBuilder) {
+        self.builder = builder
+    }
+    
+    func requestPostUpsert(item: PostDTO) -> URLRequest {
+        return builder
+            .set(method: .post)
+            .set(path: "rest/v1/Post")
+            .set(headers: [
+                "apikey": Bundle.main.apiKey,
+                "Authorization": Bundle.main.apiKey,
+                "Content-Type" : "application/json",
+                "Prefer": "resolution=merge-duplicates"
+            ])
+            .set(body: item.convertBody())
+            .build()
+    }
+    
+    func requestDeletePost(postID: String) -> URLRequest {
+        return builder
+            .set(method: .delete)
+            .set(path: "rest/v1/Post")
+            .set(queryItems: ["postId": "eq" + postID])
+            .set(headers: [
+                "apikey": Bundle.main.apiKey,
+                "Authorization": Bundle.main.apiKey
+            ])
+            .build()
+    }
+}
