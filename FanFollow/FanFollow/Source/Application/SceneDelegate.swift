1be931885197bfaef72bb5d63784301c4e571b78
미니 <cow970814@naver.com>
2023-07-03 10:33:43 +0900

    feat : 시작 ViewController 생성

--- FanFollow/FanFollow/Source/Application/SceneDelegate.swift
+++ FanFollow/FanFollow/Source/Application/SceneDelegate.swift
@@ -15,6 +15,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.backgroundColor = .systemBackground
+        window?.makeKeyAndVisible()
     }
 }
