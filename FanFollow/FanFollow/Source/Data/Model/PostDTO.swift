00abddfaf29bbec4fcae65c704d92fa10e362b68
KyoPak <hyosing92@gmail.com>
2023-07-03 21:57:52 +0900

    refactor : PostDTO CodingKeys 추가

--- FanFollow/FanFollow/Source/Data/Model/PostDTO.swift
+++ FanFollow/FanFollow/Source/Data/Model/PostDTO.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct PostDTO {
+struct PostDTO: Decodable {
     let postID: String?
     let userID: String
     
@@ -16,6 +16,11 @@ struct PostDTO {
     
     let imageURLs: [String]
     let videoURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case postID = "postId"
+        case userID, title, description, imageURLs, videoURL
+    }
     
     func convertBody() -> [String: Any] {
         return ["postId" : postID ?? UUID().uuidString,
