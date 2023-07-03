1baefd98c2ed23efdec9e518b1e8a959882bbf14
미니 <cow970814@naver.com>
2023-07-03 15:39:11 +0900

    feat : URLRequest 구성 모델 구현

---
+++ FanFollow/FanFollow/Source/NetworkManager/Model/APIError.swift
@@ -0,0 +1,9 @@
+//
+//  APIError.swift
+//  FanFollow
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+enum APIError: Error {
+    case requestBuilderFailed
+}
---
+++ FanFollow/FanFollow/Source/NetworkManager/Model/HTTPMethod.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPMethod.swift
+//  FanFollow
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+enum HTTPMethod: String {
+    case post = "POST"
+    case put = "PUT"
+    case get = "GET"
+    case delete = "DELETE"
+    case patch = "PATCH"
+}
