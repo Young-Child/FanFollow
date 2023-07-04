353082b38f4e0d6525d2274082750effafac1212
junho <junhoL1046@gmail.com>
2023-07-04 11:47:12 +0900

    feat : DefaultFollowService 구현

---
+++ FanFollow/FanFollow/Source/Data/DefaultFollowService.swift
@@ -0,0 +1,109 @@
+//
+//  DefaultFollowService.swift
+//  FanFollow
+//
+//  Created by junho lee on 2023/07/03.
+//
+
+import Foundation
+import RxSwift
+
+struct DefaultFollowService: FollowService, SupabaseService {
+    private let networkManger: Network
+
+    init(_ networkManger: Network = NetworkManager()) {
+        self.networkManger = networkManger
+    }
+
+    func fetchFollowerList(followingId: String, startRange: Int, endRange: Int) -> Observable<[FollowDTO]> {
+        guard let director = followRequestDirector() else {
+            return Observable.error(APIError.requestBuilderFailed)
+        }
+
+        let request = director.requestFollowerList(followingId, startRange: startRange, endRange: endRange)
+
+        return networkManger.data(request)
+            .compactMap { try? JSONDecoder().decode([FollowDTO].self, from: $0) }
+    }
+
+    func fetchFollowingList(followerId: String, startRange: Int, endRange: Int) -> Observable<[FollowDTO]> {
+        guard let director = followRequestDirector() else {
+            return Observable.error(APIError.requestBuilderFailed)
+        }
+
+        let request = director.requestFollowingList(followerId, startRange: startRange, endRange: endRange)
+
+        return networkManger.data(request)
+            .compactMap { try? JSONDecoder().decode([FollowDTO].self, from: $0) }
+    }
+
+    func fetchFollowerCount(followingId: String) -> RxSwift.Observable<Int> {
+        guard let director = followRequestDirector() else {
+            return Observable.error(APIError.requestBuilderFailed)
+        }
+
+        let request = director.requestFollowCount(followingId: followingId)
+
+        return networkManger.response(request)
+            .map { contentCount($0.response) }
+    }
+
+    func fetchFollowingCount(followerId: String) -> RxSwift.Observable<Int> {
+        guard let director = followRequestDirector() else {
+            return Observable.error(APIError.requestBuilderFailed)
+        }
+
+        let request = director.requestFollowCount(followerId: followerId)
+
+        return networkManger.response(request)
+            .map { contentCount($0.response) }
+    }
+
+    func checkFollow(followingId: String, followerId: String) -> RxSwift.Observable<Bool> {
+        guard let director = followRequestDirector() else {
+            return Observable.error(APIError.requestBuilderFailed)
+        }
+
+        let request = director.requestFollowCount(followingId: followingId, followerId: followerId)
+
+        return networkManger.response(request)
+            .map { contentCount($0.response) > 0 ? true : false }
+    }
+
+    func insertFollow(followerId: String, followingId: String) -> Completable {
+        guard let director = followRequestDirector() else {
+            return Completable.error(APIError.requestBuilderFailed)
+        }
+
+        let request = director.requestInsertFollow(followerId: followerId, followingId: followingId)
+
+        return networkManger.execute(request)
+    }
+
+    func deleteFollow(followerId: String, followingId: String) -> Completable {
+        guard let director = followRequestDirector() else {
+            return Completable.error(APIError.requestBuilderFailed)
+        }
+
+        let request = director.requestDeleteFollow(followerId: followerId, followingId: followingId)
+
+        return networkManger.execute(request)
+    }
+
+    private func followRequestDirector() -> FollowRequestDirector? {
+        guard let url = URL(string: baseURL) else {
+            return nil
+        }
+
+        let builder = URLRequestBuilder(baseURL: url)
+        return FollowRequestDirector(builder: builder)
+    }
+
+    private func contentCount(_ response: URLResponse) -> Int {
+        guard let response = response as? HTTPURLResponse,
+              let contentRange = response.allHeaderFields["content-range"],
+              let contentCountText = String(describing: contentRange).components(separatedBy: "/").last,
+              let contentCount = Int(contentCountText) else { return 0 }
+        return contentCount
+    }
+}
