9de680ce44ab018b018f6d65deb23f5d117178e0
junho <junhoL1046@gmail.com>
2023-07-04 19:32:43 +0900

    efactor : SupabaseConstants 사용하여 FollowRequestDirector 상수 처리

--- FanFollow/FanFollow/Source/NetworkManager/Builder/FollowRequestDirector.swift
+++ FanFollow/FanFollow/Source/NetworkManager/Builder/FollowRequestDirector.swift
@@ -16,86 +16,100 @@ struct FollowRequestDirector {
 
     func requestFollowerList(_ followingId: String, startRange: Int, endRange: Int) -> URLRequest {
         return builder
-            .set(path: "\(Constant.Text.basePath)FOLLOW")
+            .set(path: SupabaseConstants.Constants.path)
             .set(queryItems: [
-                "following_id": "eq.\(followingId)",
-                Constant.Text.select: "*,USER_INFORMATION!FOLLOW_follower_id_fkey(*)"
+                SupabaseConstants.Constants.followingId: SupabaseConstants.Base.equal + followingId,
+                SupabaseConstants.Base.select: SupabaseConstants.Constants.selectFollowerList
             ])
             .set(method: .get)
             .set(headers: [
-                Constant.Text.apikey: Bundle.main.apiKey,
-                Constant.Text.authorization: "\(Constant.Text.bearer)\(Bundle.main.apiKey)",
-                "Range": "\(startRange)-\(endRange)"
+                SupabaseConstants.Base.apikey: Bundle.main.apiKey,
+                SupabaseConstants.Base.authorization: SupabaseConstants.Base.bearer + Bundle.main.apiKey,
+                SupabaseConstants.Base.range: "\(startRange)-\(endRange)"
             ])
             .build()
     }
 
     func requestFollowingList(_ followerId: String, startRange: Int, endRange: Int) -> URLRequest {
         return builder
-            .set(path: "\(Constant.Text.basePath)FOLLOW")
+            .set(path: SupabaseConstants.Constants.path)
             .set(queryItems: [
-                "follower_id": "eq.\(followerId)",
-                Constant.Text.select: "*,USER_INFORMATION!FOLLOW_following_id_fkey(*)"
+                SupabaseConstants.Constants.followerId: SupabaseConstants.Base.equal + followerId,
+                SupabaseConstants.Base.select: SupabaseConstants.Constants.selectFollowingList
             ])
             .set(method: .get)
             .set(headers: [
-                Constant.Text.apikey: Bundle.main.apiKey,
-                Constant.Text.authorization: "\(Constant.Text.bearer)\(Bundle.main.apiKey)",
-                "Range": "\(startRange)-\(endRange)"
+                SupabaseConstants.Base.apikey: Bundle.main.apiKey,
+                SupabaseConstants.Base.authorization: SupabaseConstants.Base.bearer + Bundle.main.apiKey,
+                SupabaseConstants.Base.range: "\(startRange)-\(endRange)"
             ])
             .build()
     }
 
     func requestFollowCount(followingId: String? = nil, followerId: String? = nil) -> URLRequest {
-        var queryItems = [Constant.Text.select: "follow_id"]
+        var queryItems = [SupabaseConstants.Base.select: SupabaseConstants.Constants.followId]
         if let followingId {
-            queryItems["following_id"] = "eq.\(followingId)"
+            queryItems[SupabaseConstants.Constants.followingId] = SupabaseConstants.Base.equal + followingId
         }
         if let followerId {
-            queryItems["follower_id"] = "eq.\(followerId)"
+            queryItems[SupabaseConstants.Constants.followerId] = SupabaseConstants.Base.equal + followerId
         }
 
         return builder
-            .set(path: "\(Constant.Text.basePath)FOLLOW")
+            .set(path: SupabaseConstants.Constants.path)
             .set(queryItems: queryItems)
             .set(method: .get)
             .set(headers: [
-                Constant.Text.apikey: Bundle.main.apiKey,
-                Constant.Text.authorization: "\(Constant.Text.bearer)\(Bundle.main.apiKey)",
-                "Prefer": "count=exact"
+                SupabaseConstants.Base.apikey: Bundle.main.apiKey,
+                SupabaseConstants.Base.authorization: SupabaseConstants.Base.bearer + Bundle.main.apiKey,
+                SupabaseConstants.Base.prefer: SupabaseConstants.Constants.countExact
             ])
             .build()
     }
 
     func requestInsertFollow(followerId: String, followingId: String) -> URLRequest {
         return builder
-            .set(path: "\(Constant.Text.basePath)FOLLOW")
+            .set(path: SupabaseConstants.Constants.path)
             .set(method: .post)
             .set(headers: [
-                Constant.Text.apikey: Bundle.main.apiKey,
-                Constant.Text.authorization: "\(Constant.Text.bearer)\(Bundle.main.apiKey)",
-                "Content-Type": "application/json",
-                "Prefer": "return=minimal"
+                SupabaseConstants.Base.apikey: Bundle.main.apiKey,
+                SupabaseConstants.Base.authorization: SupabaseConstants.Base.bearer + Bundle.main.apiKey,
+                SupabaseConstants.Base.contentType: SupabaseConstants.Base.json,
+                SupabaseConstants.Base.prefer: SupabaseConstants.Constants.returnMinimal
             ])
             .set(body: [
-                "follower_id": "\(followerId)",
-                "following_id": "\(followingId)"
+                SupabaseConstants.Constants.followerId: "\(followerId)",
+                SupabaseConstants.Constants.followingId: "\(followingId)"
             ])
             .build()
     }
 
     func requestDeleteFollow(followerId: String, followingId: String) -> URLRequest {
         return builder
-            .set(path: "\(Constant.Text.basePath)FOLLOW")
+            .set(path: SupabaseConstants.Constants.path)
             .set(queryItems: [
-                "follower_id": "eq.\(followerId)",
-                "following_id": "eq.\(followingId)"
+                SupabaseConstants.Constants.followerId: SupabaseConstants.Base.equal + followerId,
+                SupabaseConstants.Constants.followingId: SupabaseConstants.Base.equal + followingId
             ])
             .set(method: .delete)
             .set(headers: [
-                Constant.Text.apikey: Bundle.main.apiKey,
-                Constant.Text.authorization: "\(Constant.Text.bearer)\(Bundle.main.apiKey)"
+                SupabaseConstants.Base.apikey: Bundle.main.apiKey,
+                SupabaseConstants.Base.authorization: SupabaseConstants.Base.bearer + Bundle.main.apiKey
             ])
             .build()
+    }
+}
+
+private extension SupabaseConstants {
+    enum Constants {
+        static let path = Base.basePath + "FOLLOW"
+        static let followingId = "following_id"
+        static let followerId = "follower_id"
+        static let followId = "follow_id"
+        static let selectFollowerList = "*,USER_INFORMATION!FOLLOW_follower_id_fkey(*)"
+        static let selectFollowingList = "*,USER_INFORMATION!FOLLOW_following_id_fkey(*)"
+        static let equalFollowingId = Base.equal + followingId
+        static let returnMinimal = "return=minimal"
+        static let countExact = Base.count + "exact"
     }
 }
