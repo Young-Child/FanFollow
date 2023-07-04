9d73dec3a3eb4721aa38c36cebfe10cad701b382
미니 <cow970814@naver.com>
2023-07-04 18:13:06 +0900

    feat : Supabase 관련 상수 추가

---
+++ FanFollow/FanFollow/Source/Util/Constants/SupabaseConstants.swift
@@ -0,0 +1,29 @@
+//
+//  SupabaseConstants.swift
+//  FanFollow
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+enum SupabaseConstants {
+    enum Base {
+        // PATH
+        static let basePath = "/rest/v1/"
+        
+        // QUERY
+        static let select = "select"
+        static let selectAll = "*"
+        static let equal = "eq."
+        static let `is` = "is."
+        static let or = "or."
+        
+        // HEADER
+        static let apikey = "apikey"
+        static let authorization = "Authorization"
+        static let bearer = "Bearer "
+        static let range = "Range"
+        static let contentType = "Content-Type"
+        static let json = "application/json"
+        static let prefer = "Prefer"
+        static let count = "count="
+    }
+}
