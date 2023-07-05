0e5b6501e70b51a90f088ff4c7c81ff989adc257
미니 <cow970814@naver.com>
2023-07-06 01:12:07 +0900

    test : StubURLSession 타입 MockURLSessionDataTask 프로토콜 타입 반환하도록 변경

--- FanFollow/NetworkManagerTests/Stub/StubURLSession.swift
+++ FanFollow/NetworkManagerTests/Stub/StubURLSession.swift
@@ -8,46 +8,49 @@ import Foundation
 
 @testable import FanFollow
 
-class StubURLSession<T: MockDataType>: URLSessionType {
-    private var mock: T
-    private var isSuccess: Bool
-    private var sessionDataTask: MockURLSessionDataTask?
+class StubURLSession: URLSessionType {
+    typealias Response = (data: Data?, response: URLResponse?, error: Error?)
     
-    init(mock: T, isSuccess: Bool = true) {
-        self.mock = mock
-        self.isSuccess = isSuccess
+    private let response: Response
+    
+    init(response: Response) {
+        self.response = response
     }
     
     func dataTask(
         with request: URLRequest,
         completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
     ) -> URLSessionDataTaskType {
-        let successResponse = HTTPURLResponse(
-            url: request.url!,
-            statusCode: 200,
-            httpVersion: "2",
-            headerFields: nil
-        )
-        
-        let failureResponse = HTTPURLResponse(
-            url: request.url!,
-            statusCode: 404,
-            httpVersion: "2",
-            headerFields: nil
-        )
-        
-        let dataTask = MockURLSessionDataTask { [weak self] in
-            guard let self = self else { return }
-            
-            if self.isSuccess {
-                completionHandler(mock.sampleData, successResponse, nil)
-            } else {
-                completionHandler(nil, failureResponse, NetworkError.unknown)
-            }
+        return MockURLSessionDataTask {
+            completionHandler(
+                self.response.data,
+                self.response.response,
+                self.response.error
+            )
         }
+    }
+}
+
+extension StubURLSession {
+    static func make(url: String, mock: MockData, statusCode: Int) -> StubURLSession {
+        let stubURLSession: StubURLSession = {
+            let response = HTTPURLResponse(
+                url: URL(string: url)!,
+                statusCode: statusCode,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            
+            let mockResponse: Response = (
+                data: mock.sampleData,
+                response: response,
+                error: statusCode == 200 ? nil : NetworkError.unknown
+            )
+            
+            let session = StubURLSession(response: mockResponse)
+            return session
+        }()
         
-        self.sessionDataTask = dataTask
-        
-        return dataTask
+        return stubURLSession
     }
 }
