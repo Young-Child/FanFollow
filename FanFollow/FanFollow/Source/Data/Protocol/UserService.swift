6cf1a3a104d91709419b93cb3b8fad75a040c20f
junho <junhoL1046@gmail.com>
2023-07-05 17:02:16 +0900

    feat: UserService 구현

---
+++ FanFollow/FanFollow/Source/Data/Protocol/UserService.swift
@@ -0,0 +1,20 @@
+//
+//  UserService.swift
+//  FanFollow
+//
+//  Created by junho lee on 2023/07/05.
+//
+
+import Foundation
+import RxSwift
+
+protocol UserService: SupabaseService {
+    func fetchUserInformations(jobCategory: Int, startRange: Int, endRange: Int) -> Observable<[UserInformationDTO]>
+    func fetchUserInformations(nickName: String, startRange: Int, endRange: Int) -> Observable<[UserInformationDTO]>
+    func fetchUserInformation(for userID: String) -> Observable<UserInformationDTO>
+    func upsertUserInformation(
+        userID: String, name: String, nickName: String, profilePath: String?, jobCategory: Int?,
+        links: [String]?, introduce: String?, isCreator: Bool, createdAt: String
+    ) -> Completable
+    func deleteUserInformation(userID: String) -> Completable
+}
