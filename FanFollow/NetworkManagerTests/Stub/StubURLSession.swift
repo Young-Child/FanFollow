5bb5156c36eaf05db42d1961296b3b6e552f539a
미니 <cow970814@naver.com>
2023-07-05 23:54:33 +0900

    test : Stub한 URLSession타입 구현

---
+++ FanFollow/NetworkManagerTests/Stub/StubURLSession.swift
@@ -0,0 +1,69 @@
+//
+//  StubURLSession.swift
+//  NetworkManagerTests
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import Foundation
+
+@testable import FanFollow
+
+protocol URLSessionType {
+    func dataTask(
+        with request: URLRequest,
+        completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
+    ) -> URLSessionDataTaskProtocol
+}
+
+extension URLSession: URLSessionType {
+    func dataTask(
+        with request: URLRequest,
+        completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
+    ) -> URLSessionDataTaskProtocol {
+        return dataTask(with: request, completionHandler: completionHandler) as URLSessionDataTask
+    }
+}
+
+class StubURLSession<T: MockDataType>: URLSessionType {
+    private var mock: T
+    private var isSuccess: Bool
+    private var sessionDataTask: MockURLSessionDataTask?
+    
+    init(mock: T, isSuccess: Bool = true) {
+        self.mock = mock
+        self.isSuccess = isSuccess
+    }
+    
+    func dataTask(
+        with request: URLRequest,
+        completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
+    ) -> URLSessionDataTaskProtocol {
+        let successResponse = HTTPURLResponse(
+            url: request.url!,
+            statusCode: 200,
+            httpVersion: "2",
+            headerFields: nil
+        )
+        
+        let failureResponse = HTTPURLResponse(
+            url: request.url!,
+            statusCode: 404,
+            httpVersion: "2",
+            headerFields: nil
+        )
+        
+        let dataTask = MockURLSessionDataTask { [weak self] in
+            guard let self = self else { return }
+            
+            if self.isSuccess {
+                completionHandler(mock.sampleData, successResponse, nil)
+            } else {
+                completionHandler(nil, failureResponse, NetworkError.unknown)
+            }
+        }
+        
+        self.sessionDataTask = dataTask
+        
+        return dataTask
+    }
+}
