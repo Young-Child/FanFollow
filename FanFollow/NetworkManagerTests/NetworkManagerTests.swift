1727e1db630aea41f1f9ac25a9ad8adf110bbe44
미니 <cow970814@naver.com>
2023-07-06 01:27:00 +0900

    test : test 케이스 추가

--- FanFollow/NetworkManagerTests/NetworkManagerTests.swift
+++ FanFollow/NetworkManagerTests/NetworkManagerTests.swift
@@ -29,8 +29,8 @@ final class NetworkManagerTests: XCTestCase {
         sample = nil
     }
     
-    /// 네트워크없이 NetworkManager 내부의 response 메서드가 정상적인 데이터를 전달하는지 테스트
-    func test_FetchingDataWithDataExistWhenStatusCode200() {
+    /// 네트워크없이 NetworkManager 내부의 data 메서드가 정상적인 데이터를 전달하는지 테스트
+    func test_FetchingDataWithDataExistWhenStatusCode200() throws {
         //given
         let stubURLSession = StubURLSession.make(
             url: url,
@@ -41,21 +41,23 @@ final class NetworkManagerTests: XCTestCase {
         let sut = NetworkManager(session: stubURLSession)
         
         // when
-        let fetchResult = sut.response(URLRequest(url: URL(staticString: self.url)))
+        let fetchResult = sut.data(URLRequest(url: URL(staticString: self.url)))
         
         //then
-        fetchResult.map(\.data)
+        fetchResult
             .subscribe(onNext: { data in
                 let result = try! JSONDecoder().decode(ChatDTO.self, from: data)
                 let expected = try! JSONDecoder().decode(ChatDTO.self, from: self.sample.sampleData)
                 
                 XCTAssertEqual(result.chatID, expected.chatID)
+            }, onError: { _ in
+                XCTAssertThrowsError("We expected onNext Event, But Sending onError Event")
             })
             .disposed(by: disposeBag)
     }
     
-    /// 네트워크없이 NetworkManager 내부의 response 메서드가 정상적으로 에러를 전달하는지 테스트
-    func test_FetchingDataWithDataExistWhenStatusCodeNot200() {
+    /// 네트워크없이 NetworkManager 내부의 data 메서드가 정상적으로 에러를 전달하는지 테스트
+    func test_FetchingDataWithDataExistWhenStatusCodeNot200() throws {
         let stubURLSession = StubURLSession.make(
             url: url,
             mock: sample,
@@ -64,12 +66,56 @@ final class NetworkManagerTests: XCTestCase {
         
         let sut = NetworkManager(session: stubURLSession)
         
-        let fetchResult = sut.response(URLRequest(url: URL(staticString: self.url)))
+        let fetchResult = sut.data(URLRequest(url: URL(staticString: self.url)))
         
-        fetchResult.subscribe(onError: { error in
+        fetchResult.subscribe(onNext: { _ in
+            XCTAssertThrowsError("We expected onError Event, But Sending onNext Event")
+        }, onError: { error in
             let error = error as? NetworkError
             let expected = NetworkError.unknown
             
+            XCTAssertEqual(error, expected)
+        })
+        .disposed(by: disposeBag)
+    }
+    
+    /// 네트워크 없이 NetworkManager 내부의 execute 메서드가 정상적인값을 반환하는 것에 대한 테스트
+    func test_FetchingDataWithDataNoExistWhenStatusCode200() throws {
+        let stubURLSession = StubURLSession.make(
+            url: url,
+            mock: sample,
+            statusCode: 200
+        )
+        
+        let sut = NetworkManager(session: stubURLSession)
+        
+        let fetchResult = sut.execute(URLRequest(url: URL(staticString: self.url)))
+        
+        fetchResult.subscribe(onCompleted: {
+            XCTAssert(true)
+        }, onError: { _ in
+            XCTAssertThrowsError("We expected onCompleted Event, But Sending onError Event")
+        })
+        .disposed(by: disposeBag)
+    }
+    
+    /// 네트워크 없이 NetworkManager 내부의 execute 메서드가 에러를 정상적으로 반환하는 지에 대한 테스트
+    func test_FetchingDataWithDataNoExistWhenStatusCodeNot200() throws {
+        let stubURLSession = StubURLSession.make(
+            url: url,
+            mock: sample,
+            statusCode: 400
+        )
+        
+        let sut = NetworkManager(session: stubURLSession)
+        
+        let fetchResult = sut.execute(URLRequest(url: URL(staticString: self.url)))
+        
+        fetchResult.subscribe(onCompleted: {
+            XCTAssertThrowsError("We expected onError Event, But Sending onCompleted Event")
+        }, onError: { error in
+            let error = error as? NetworkError
+            let expected = NetworkError.unknown
             XCTAssertEqual(error, expected)
         })
         .disposed(by: disposeBag)
