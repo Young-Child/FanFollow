9d1222985326add8773c1d5a5735f79d5a993a15
미니 <cow970814@naver.com>
2023-07-07 02:12:31 +0900

    test : CI 테스트 위한 설정 변경

--- FanFollow/ServiceTests/ChatServiceTests.swift
+++ FanFollow/ServiceTests/ChatServiceTests.swift
@@ -148,7 +148,7 @@ final class ChatServiceTests: XCTestCase {
         
         switch result {
         case .completed:
-            XCTAssert(true)
+            XCTAssertThrowsError(NetworkError.unknown, "We expected Completed Event, But Occur Error Event")
         case .failed(_, let error):
             XCTAssertThrowsError(error, "We expected Completed Event, But Occur Error Event")
         }
