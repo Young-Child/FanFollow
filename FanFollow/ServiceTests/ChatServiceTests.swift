ecbcdac1587a5fa26bcc271d2ce1d081be1981d2
미니 <cow970814@naver.com>
2023-07-06 23:40:51 +0900

    feat : Chat Service 타입 테스트 파일 생성

---
+++ FanFollow/ServiceTests/ChatServiceTests.swift
@@ -0,0 +1,26 @@
+//
+//  ChatServiceTests.swift
+//  ServiceTests
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import XCTest
+
+import RxSwift
+import RxBlocking
+import RxTest
+import RxRelay
+
+final class ChatServiceTests: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+    }
+    
+    func test_example() {
+        XCTAssertEqual(true, true)
+    }
+}
