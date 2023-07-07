eb1c8188a6dc98d09871c1a24fd15c6a387dcb5a
미니 <cow970814@naver.com>
2023-07-07 11:17:41 +0900

    test : 정상적인 값을 통해서 채팅방 떠나기 메서드가 에러를 방출하는지에 대한 테스트 케이스 추가

--- FanFollow/ServiceTests/ChatServiceTests.swift
+++ FanFollow/ServiceTests/ChatServiceTests.swift
@@ -17,7 +17,7 @@ final class ChatServiceTests: XCTestCase {
     private var networkManager: StubNetworkManager!
     
     override func setUpWithError() throws {
-        let url = URL(string: "https://qacasllvaxvrtwbkiavx.supabase.co/rest/v1/CHAT_ROOM?select=chat_id")!
+        let url = URL(string: "https://qacasllvaxvrtwbkiavx.supabase.co/rest/v1/CHAT_ROOM")!
         
         self.successResponse = HTTPURLResponse(
             url: url,
@@ -65,7 +65,7 @@ final class ChatServiceTests: XCTestCase {
         XCTAssertEqual(result, true)
     }
     
-    /// 비정상적인 사용자ID를 전달하였을 때 에러를 방출하는지에 대한 테스트
+    /// 정상적인 사용자ID를 전달하였을 때 에러를 방출하는지에 대한 테스트
     func test_FetchChattingListThrowErrorWhenSendWrongData() throws {
         // given
         let userID = ""
@@ -109,7 +109,7 @@ final class ChatServiceTests: XCTestCase {
         }
     }
     
-    /// 비정상적인 사용자ID를 전달하였을 때 채팅방 생성 에러 이벤트가 방출하는지에 대한 테스트
+    /// 정상적인 사용자ID를 전달하였을 때 채팅방 생성 에러 이벤트가 방출하는지에 대한 테스트
     func test_CreateNewChatRoomIsErrorWhenSendWrongData() throws {
         // given
         let fanID = "5b587434-438c-49d8-ae3c-88bb27a891d4"
@@ -151,6 +151,32 @@ final class ChatServiceTests: XCTestCase {
             XCTAssert(true)
         case .failed(_, let error):
             XCTAssertThrowsError(error, "We expected Completed Event, But Occur Error Event")
+        }
+    }
+    
+    /// 정상적인 데이터를 전달하였을 때 채팅방 떠나기 함수가 에러 이벤트를 방출하는지에 대한 테스트
+    func test_LeaveChatRoomIsErrorWhenSendWrongData() throws {
+        // given
+        let chatID = "3538b47a-1113-4aff-96d9-6e2ec4b37d46"
+        let fanID = "5b587434-438c-49d8-ae3c-88bb27a891d4"
+        networkManager.response = successResponse
+        networkManager.error = NetworkError.unknown
+        
+        //when
+        let chatService = DefaultChatService(networkManager: self.networkManager)
+        let observable = chatService.leaveChatRoom(to: chatID, userID: fanID, isCreator: false)
+        
+        //then
+        let result = observable.toBlocking().materialize()
+        
+        switch result {
+        case .completed:
+            XCTAssertThrowsError(
+                NetworkError.unknown,
+                "We expected Error Event, But Occur OnCompleted Event"
+            )
+        case .failed:
+            XCTAssertTrue(true)
         }
     }
 }
