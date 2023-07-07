95af6a2abb08da46e1d0c0975eaf56f12d438028
미니 <cow970814@naver.com>
2023-07-07 11:20:25 +0900

    test : 정상적인 값을 통해서 채팅방 삭제 케이스 추가

--- FanFollow/ServiceTests/ChatServiceTests.swift
+++ FanFollow/ServiceTests/ChatServiceTests.swift
@@ -159,12 +159,58 @@ final class ChatServiceTests: XCTestCase {
         // given
         let chatID = "3538b47a-1113-4aff-96d9-6e2ec4b37d46"
         let fanID = "5b587434-438c-49d8-ae3c-88bb27a891d4"
-        networkManager.response = successResponse
+        networkManager.response = failureResponse
         networkManager.error = NetworkError.unknown
         
         //when
         let chatService = DefaultChatService(networkManager: self.networkManager)
         let observable = chatService.leaveChatRoom(to: chatID, userID: fanID, isCreator: false)
+        
+        //then
+        let result = observable.toBlocking().materialize()
+        
+        switch result {
+        case .completed:
+            XCTAssertThrowsError(
+                NetworkError.unknown,
+                "We expected Error Event, But Occur OnCompleted Event"
+            )
+        case .failed:
+            XCTAssertTrue(true)
+        }
+    }
+    
+    /// 정상적인 데이터를 전달하였을 때 채팅방 삭제 함수가 완료 이벤트를 방출하는지에 대한 테스트
+    func test_DeleteChatRoomIsCompletedWhenSendCorrectData() throws {
+        // given
+        let chatID = "3538b47a-1113-4aff-96d9-6e2ec4b37d46"
+        networkManager.response = successResponse
+        
+        //when
+        let chatService = DefaultChatService(networkManager: self.networkManager)
+        let observable = chatService.deleteChatRoom(to: chatID)
+        
+        //then
+        let result = observable.toBlocking().materialize()
+        
+        switch result {
+        case .completed:
+            XCTAssert(true)
+        case .failed(_, let error):
+            XCTAssertThrowsError(error, "We expected Completed Event, But Occur Error Event")
+        }
+    }
+    
+    /// 정상적인 데이터를 전달하였을 때 채팅방 떠나기 함수가 에러 이벤트를 방출하는지에 대한 테스트
+    func test_LeaveChatRoomIsErrorWhenSendWrongData() throws {
+        // given
+        let chatID = "3538b47a-1113-4aff-96d9-6e2ec4b37d46"
+        networkManager.response = failureResponse
+        networkManager.error = NetworkError.unknown
+        
+        //when
+        let chatService = DefaultChatService(networkManager: self.networkManager)
+        let observable = chatService.deleteChatRoom(to: chatID)
         
         //then
         let result = observable.toBlocking().materialize()
