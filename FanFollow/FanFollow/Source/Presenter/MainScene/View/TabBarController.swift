1f23ef5357de1c5eaaf7a1df81d150bfc5ef6b31
미니 <cow970814@naver.com>
2023-07-11 10:35:31 +0900

    feat : Main Scene 폴더 생성 및 TabBarController 생성

---
+++ FanFollow/FanFollow/Source/Presenter/MainScene/View/TabBarController.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarController.swift
+//  FanFollow
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
