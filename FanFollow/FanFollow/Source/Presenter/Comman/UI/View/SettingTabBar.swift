e45546c6ce9ab14649a4f082dd742a36e58b648b
미니 <cow970814@naver.com>
2023-07-12 15:17:55 +0900

    feat : Setting Tab Bar 타입 구현

--- FanFollow/FanFollow/Source/Presenter/Comman/UI/View/SettingTabBar.swift
+++ FanFollow/FanFollow/Source/Presenter/Comman/UI/View/SettingTabBar.swift
@@ -3,6 +3,23 @@
 //  FanFollow
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
 import Foundation
+
+final class SettingTabBar: TabBar {
+    private enum SettingTabItem: Int, TabItem {
+        case setting
+        case feedManage
+        
+        var description: String {
+            switch self {
+            case .setting:      return "설정"
+            case .feedManage:   return "피드 관리"
+            }
+        }
+    }
+    
+    convenience init() {
+        self.init(tabItems: SettingTabItem.allCases)
+    }
+}
