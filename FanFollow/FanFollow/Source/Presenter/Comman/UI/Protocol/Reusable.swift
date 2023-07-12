51f03c14441814150c027464cb583735aa241ae2
미니 <cow970814@naver.com>
2023-07-12 18:01:15 +0900

    feat : Cell 타입 reuse 프로토콜 생성 및 TableCell 타입 적용

---
+++ FanFollow/FanFollow/Source/Presenter/Comman/UI/Protocol/Reusable.swift
@@ -0,0 +1,19 @@
+//
+//  Reusable.swift
+//  FanFollow
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import UIKit
+
+protocol Reusable: AnyObject {
+    static var reuseIdentifier: String { get }
+}
+
+extension Reusable {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell: Reusable { }
