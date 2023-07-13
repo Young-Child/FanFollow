4424e1df5a3ade9daff304764c28a88e727797bf
junho <junhoL1046@gmail.com>
2023-07-13 15:58:03 +0900

    test : StubLikeRepository 구현
    (cherry picked from commit c7601c7973f29abbe58baf8d90a28de9c835d01d)

---
+++ FanFollow/UseCaseTests/Stub/StubLikeRepository.swift
@@ -0,0 +1,49 @@
+//
+//  StubLikeRepository.swift
+//  UseCaseTests
+//
+//  Created by junho lee on 2023/07/13.
+//
+
+import RxSwift
+
+@testable import FanFollow
+
+final class StubLikeRepository: LikeRepository {
+    var count = 0
+    var error: Error?
+
+    func fetchPostLikeCount(postID: String, userID: String?) -> Observable<Int> {
+        return Observable.create { observer in
+            if let error = self.error {
+                observer.onError(error)
+            } else {
+                observer.onNext(self.count)
+                observer.onCompleted()
+            }
+            return Disposables.create()
+        }
+    }
+
+    func createPostLike(postID: String, userID: String) -> Completable {
+        return Completable.create { observer in
+            if let error = self.error {
+                observer(.error(error))
+            } else {
+                observer(.completed)
+            }
+            return Disposables.create()
+        }
+    }
+
+    func deletePostLike(postID: String, userID: String) -> Completable {
+        return Completable.create { observer in
+            if let error = self.error {
+                observer(.error(error))
+            } else {
+                observer(.completed)
+            }
+            return Disposables.create()
+        }
+    }
+}
