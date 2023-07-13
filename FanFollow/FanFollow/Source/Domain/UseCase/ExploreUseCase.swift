c110c7ddeb4a1bc50058e91c296046f8612796f0
KyoPak <hyosing92@gmail.com>
2023-07-13 10:58:49 +0900

    feat : ExploreUseCase 내부 직업카테고리에 따른 fetchRandomCreators() 메서드 구현 및 전체 직업군 가져오는 fetchRandomAllCreators() 구현

--- FanFollow/FanFollow/Source/Domain/UseCase/ExploreUseCase.swift
+++ FanFollow/FanFollow/Source/Domain/UseCase/ExploreUseCase.swift
@@ -6,15 +6,47 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol ExploreUseCase: AnyObject {
-    
+    func fetchRandomCreators(jobCategory: JobCategory) -> Observable<[Creator]>
+    func fetchRandomAllCreators() -> Observable<[String: [Creator]]>
 }
 
 final class DefaultExploreUseCase: ExploreUseCase {
-    private let useInformationRepository: UserInformationRepository
+    private let userInformationRepository: UserInformationRepository
     
-    init(useInformationRepository: UserInformationRepository) {
-        self.useInformationRepository = useInformationRepository
+    init(userInformationRepository: UserInformationRepository) {
+        self.userInformationRepository = userInformationRepository
+    }
+    
+    func fetchRandomCreators(jobCategory: JobCategory) -> Observable<[Creator]> {
+        let creatorList = userInformationRepository.fetchRandomCreatorInformations(jobCategory: jobCategory)
+        
+        return creatorList.map { userInformationDTOList in
+            userInformationDTOList.map { userInformationDTO in
+                return Creator(userInformationDTO)
+            }
+        }
+    }
+    
+    func fetchRandomAllCreators() -> Observable<[String: [Creator]]> {
+        var categoryCreator: [String: [Creator]] = [:]
+        let allJobs = JobCategory.allCases
+        
+        let fetchCreatorsObservables = allJobs.map { jobCategory in
+            return fetchRandomCreators(jobCategory: jobCategory)
+                .map { creators in
+                    return (jobCategory, creators)
+                }
+        }
+        
+        return Observable.zip(fetchCreatorsObservables)
+            .map { results in
+                for (jobCategory, creators) in results {
+                    categoryCreator[jobCategory.categoryName] = creators
+                }
+                return categoryCreator
+            }
     }
 }
