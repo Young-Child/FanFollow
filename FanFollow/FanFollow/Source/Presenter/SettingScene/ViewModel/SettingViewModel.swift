67b720bb230a9bc0095fdd92095afc386c4b075d
미니 <cow970814@naver.com>
2023-07-14 00:19:37 +0900

    feat : Setting View Model 파일 생성

---
+++ FanFollow/FanFollow/Source/Presenter/SettingScene/ViewModel/SettingViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  SettingViewModel.swift
+//  FanFollow
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import RxDataSources
+import RxSwift
+
+final class SettingViewModel: ViewModel {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    var disposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+    
+}
