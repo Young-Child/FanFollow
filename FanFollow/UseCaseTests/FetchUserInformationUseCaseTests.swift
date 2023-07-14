fd32635c32f22355aadd09b95f42664332b1e4cb
junho <junhoL1046@gmail.com>
2023-07-14 10:43:24 +0900

    test : 팬/크리에이터인 경우를 구분하여 테스트 다시 작성

--- FanFollow/UseCaseTests/FetchUserInformationUseCaseTests.swift
+++ FanFollow/UseCaseTests/FetchUserInformationUseCaseTests.swift
@@ -29,19 +29,40 @@ final class FetchUserInformationUseCaseTests: XCTestCase {
         try super.tearDownWithError()
     }
 
-    /// 정상적인 조건에서 fetchUserInformation가 제대로 동작하는 지 테스트
-    func test_fetchUserInformationInNormalCondition() {
+    /// 정상적인 조건에서 fetchUserInformation가 팬 정보를 제대로 가져오는지 테스트
+    func test_fetchUserInformationOfFanInNormalCondition() {
         // given
-        userInformationRepository.userInformation = TestData.userInformation
+        userInformationRepository.userInformation = TestData.fanInformation
         userInformationRepository.error = nil
-        let userID = TestData.userID
+        let userID = TestData.fanID
 
         // when
         let observable = sut.fetchUserInformation(for: userID)
 
         // then
         observable.subscribe(onNext: { value in
+            XCTAssertTrue(value is Fan)
             XCTAssertEqual(value.id, userID)
+        }, onError: { error in
+            XCTFail(error.localizedDescription)
+        })
+        .disposed(by: disposeBag)
+    }
+
+    /// 정상적인 조건에서 fetchUserInformation가 크리에이터 정보를 제대로 가져오는지 테스트
+    func test_fetchUserInformationOfCreatorInNormalCondition() {
+        // given
+        userInformationRepository.userInformation = TestData.creatorInformation
+        userInformationRepository.error = nil
+        let creatorID = TestData.creatorID
+
+        // when
+        let observable = sut.fetchUserInformation(for: creatorID)
+
+        // then
+        observable.subscribe(onNext: { value in
+            XCTAssertTrue(value is Creator)
+            XCTAssertEqual(value.id, creatorID)
         }, onError: { error in
             XCTFail(error.localizedDescription)
         })
@@ -51,9 +72,9 @@ final class FetchUserInformationUseCaseTests: XCTestCase {
     /// 에러가 발생하는 조건에서 fetchUserInformation가 에러를 반환하는 지 테스트
     func test_fetchUserInformationInErrorCondition() {
         // given
-        userInformationRepository.userInformation = TestData.userInformation
+        userInformationRepository.userInformation = TestData.fanInformation
         userInformationRepository.error = TestData.error
-        let userID = TestData.userID
+        let userID = TestData.fanID
 
         // when
         let observable = sut.fetchUserInformation(for: userID)
@@ -70,7 +91,7 @@ final class FetchUserInformationUseCaseTests: XCTestCase {
 
 extension FetchUserInformationUseCaseTests {
     private enum TestData {
-        static let userInformation = UserInformationDTO(
+        static let fanInformation = UserInformationDTO(
             userID: "a0728b90-0172-4552-9b31-1f3cab84900b",
             nickName: "하나",
             profilePath: nil,
@@ -80,7 +101,18 @@ extension FetchUserInformationUseCaseTests {
             isCreator: false,
             createdAt: "2023-07-05 10:25:45.474385+00"
         )
-        static let userID = "a0728b90-0172-4552-9b31-1f3cab84900b"
+        static let creatorInformation = UserInformationDTO(
+            userID: "5b260fc8-50ef-4f5b-8315-a19e3c69dfc2",
+            nickName: "나미",
+            profilePath: "www.naver.com",
+            jobCategory: 1,
+            links: ["www.naver.com","www.google.com"],
+            introduce: "사랑의 몸이 하여도 것이다.",
+            isCreator: true,
+            createdAt: "2023-07-04 08:40:02.189472+00"
+        )
+        static let fanID = "a0728b90-0172-4552-9b31-1f3cab84900b"
+        static let creatorID = "5b260fc8-50ef-4f5b-8315-a19e3c69dfc2"
         static let error = NetworkError.unknown
     }
 }
