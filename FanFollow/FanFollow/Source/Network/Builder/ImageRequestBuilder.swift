4a4b1d66dc7400ca498dd5f0eac414956122a675
미니 <cow970814@naver.com>
2023-07-14 22:04:42 +0900

    feat : Image Request Builder 타입 구현

---
+++ FanFollow/FanFollow/Source/Network/Builder/ImageRequestBuilder.swift
@@ -0,0 +1,33 @@
+//
+//  ImageRequestBuilder.swift
+//  FanFollow
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import Foundation
+
+struct ImageRequestBuilder {
+    private let builder: URLRequestBuilder
+    
+    init(builder: URLRequestBuilder) {
+        self.builder = builder
+    }
+    
+    func requestSaveImage(path: String, image: Data) -> URLRequest {
+        return builder
+            .set(path: SupabaseConstants.Constants.path + path)
+            .set(method: .post)
+            .set(headers: [
+                SupabaseConstants.Base.apikey: Bundle.main.apiKey,
+                SupabaseConstants.Base.authorization: SupabaseConstants.Base.bearer + Bundle.main.apiKey
+            ])
+            .set(body: .multipart(data: image))
+            .build()
+    }
+}
+
+private extension SupabaseConstants {
+    enum Constants {
+        static let path = "/storage/v1/object/"
+    }
+}
