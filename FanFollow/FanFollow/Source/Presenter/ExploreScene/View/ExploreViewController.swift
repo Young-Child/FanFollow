11978aa32bced3a637ea22e5d99df0605ce32133
KyoPak <hyosing92@gmail.com>
2023-07-17 13:07:45 +0900

    config : ExploreView, ExploreViewModel 파일 생성

---
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/View/ExploreViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ExploreViewController.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/07/17.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/ViewModel/ExploreViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ExploreViewModel.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/07/17.
+//
+
+import Foundation
+
+final class ExploreViewModel {
+    
+}
