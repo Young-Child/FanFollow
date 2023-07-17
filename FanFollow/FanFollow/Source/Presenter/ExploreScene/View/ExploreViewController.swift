0780b3ca5d22693b4a0cdc41bf28382af998a3ab
KyoPak <hyosing92@gmail.com>
2023-07-17 19:28:54 +0900

    feat : ExploreViewController 내부 UICollectionView Layout 생성하는 메서드 구현

--- FanFollow/FanFollow/Source/Presenter/ExploreScene/View/ExploreViewController.swift
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/View/ExploreViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 import RxSwift
 
 final class ExploreViewController: UIViewController {
+    // View Properties
+    private let exploreCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewLayout()
+    ).then {
+        $0.register(CategoryCell.self, forCellWithReuseIdentifier: CategoryCell.reuseIdentifier)
+        $0.register(CreatorCell.self, forCellWithReuseIdentifier: CreatorCell.reuseIdentifier)
+        $0.backgroundColor = .clear
+    }
+    
     // Properties
     private let viewModel: ExploreViewModel
     private let disposeBag = DisposeBag()
@@ -26,5 +36,58 @@ final class ExploreViewController: UIViewController {
     // Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+// UICollectionView Layout Method
+extension ExploreViewController {
+    private func createCategorySection(item: NSCollectionLayoutItem) -> NSCollectionLayoutSection {
+        let categoryGroupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalHeight(0.3),
+            heightDimension: .fractionalHeight(0.5)
+        )
+        
+        let categoryGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: categoryGroupSize,
+            subitems: [item]
+        )
+        
+        let categorySection = NSCollectionLayoutSection(group: categoryGroup)
+        categorySection.interGroupSpacing = 10
+        
+        return categorySection
+    }
+    
+    private func createCreatorSection(item: NSCollectionLayoutItem) -> NSCollectionLayoutSection {
+        let creatorGroupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalHeight(0.3),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        
+        let creatorGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: creatorGroupSize,
+            subitems: [item]
+        )
+        
+        let creatorSection = NSCollectionLayoutSection(group: creatorGroup)
+        creatorSection.orthogonalScrollingBehavior = .continuous
+        
+        return creatorSection
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let commonItemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        
+        let commonItem = NSCollectionLayoutItem(layoutSize: commonItemSize)
+        
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, _ in
+            return sectionIndex == .zero ?
+            self.createCategorySection(item: commonItem) : self.createCreatorSection(item: commonItem)
+        }
+        
+        return layout
     }
 }
