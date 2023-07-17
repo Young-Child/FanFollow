c3e9fc28cb3252dacec9f02d3216b7d92047d205
KyoPak <hyosing92@gmail.com>
2023-07-17 16:03:30 +0900

    feat : CreatorCell SnapKit Import 하여 Constraint 구현 및 필요한 UI SetMethod 구현

--- FanFollow/FanFollow/Source/Presenter/ExploreScene/View/CreatorCell.swift
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/View/CreatorCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 final class CreatorCell: UICollectionViewCell {
     // View Properties
@@ -14,7 +15,6 @@ final class CreatorCell: UICollectionViewCell {
         $0.layer.cornerRadius = 25
         $0.contentMode = .scaleAspectFill
         $0.backgroundColor = UIColor(named: "SecondaryColor")
-        $0.translatesAutoresizingMaskIntoConstraints = false
     }
     
     private let nickNameLabel = UILabel().then {
@@ -22,15 +22,54 @@ final class CreatorCell: UICollectionViewCell {
         $0.textAlignment = .center
         $0.textColor = UIColor(named: "AccentColor")
         $0.font = .preferredFont(forTextStyle: .body)
-        $0.translatesAutoresizingMaskIntoConstraints = false
     }
     
     // Initializer
     override init(frame: CGRect) {
         super.init(frame: .zero)
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// UI Method
+extension CreatorCell {
+    func configureCell(nickName: String, userID: String) {
+        guard let defaultImage = UIImage(systemName: "person") else { return }
+        nickNameLabel.text = nickName
+        profileImageView.setImageKF(
+            to: "https://qacasllvaxvrtwbkiavx.supabase.co/storage/v1/object/ProfileImage/\(userID)/profileImage.png",
+            failureImage: defaultImage
+        )
+    }
+}
+
+// Configure UI
+private extension CreatorCell {
+    func configureUI() {
+        configureHierarchy()
+        makeConstraints()
+    }
+    
+    func configureHierarchy() {
+        [profileImageView, nickNameLabel].forEach { contentView.addSubview($0) }
+    }
+    
+    func makeConstraints() {
+        profileImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(15)
+            $0.leading.equalToSuperview().offset(15)
+            $0.trailing.equalToSuperview().offset(-15)
+            $0.height.equalTo(profileImageView.snp.width)
+        }
+        
+        nickNameLabel.snp.makeConstraints {
+            $0.top.equalTo(profileImageView.snp.bottom).offset(10)
+            $0.bottom.equalToSuperview().offset(-10)
+            $0.centerX.equalToSuperview()
+        }
     }
 }
