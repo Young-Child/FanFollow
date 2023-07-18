5e4df378ee91980372efcfc82151be035ac3911e
KyoPak <hyosing92@gmail.com>
2023-07-18 18:12:51 +0900

    feat : ExploreTabBarController에 SearchButton 추가 및 위치 조정

--- FanFollow/FanFollow/Source/Presenter/Comman/UI/Controller/ExploreTabBarController.swift
+++ FanFollow/FanFollow/Source/Presenter/Comman/UI/Controller/ExploreTabBarController.swift
@@ -8,7 +8,33 @@
 import UIKit
 
 final class ExploreTabBarController: TopTabBarController<ExploreTapItem> {
+    private let searchButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        $0.tintColor = .label
+        $0.backgroundColor = .clear
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+    }
+}
+
+private extension ExploreTabBarController {
+    func configureUI() {
+        configureHierarchy()
+        makeConstraints()
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(searchButton)
+    }
+    
+    func makeConstraints() {
+        searchButton.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.trailing.equalToSuperview().offset(-20)
+        }
     }
 }
