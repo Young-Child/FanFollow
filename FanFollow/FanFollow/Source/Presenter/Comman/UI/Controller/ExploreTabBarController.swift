af39805732eaf9b79f5f816ca7f36e47b2fb8398
KyoPak <hyosing92@gmail.com>
2023-07-18 17:52:31 +0900

    feat : ExploreTabBarController 생성 및 TobTabBarController 보이게끔 수정

---
+++ FanFollow/FanFollow/Source/Presenter/Comman/UI/Controller/ExploreTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  ExploreTabBarController.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/07/18.
+//
+
+import UIKit
+
+final class ExploreTabBarController: TopTabBarController<ExploreTapItem> {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
