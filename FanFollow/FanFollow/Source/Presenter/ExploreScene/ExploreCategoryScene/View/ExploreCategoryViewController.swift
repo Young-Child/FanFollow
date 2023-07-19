0072c09e680dfb3c7d6a5132f69ab155eae521c9
KyoPak <hyosing92@gmail.com>
2023-07-19 14:55:17 +0900

    config : ExploreScene 폴더 내부 ExploreMain과 ExploreCategory 폴더 분리 및 파일 생성

---
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/ExploreCategoryScene/View/ExploreCategoryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ExploreCategoryViewController.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/07/19.
+//
+
+import UIKit
+
+final class ExploreCategoryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/ExploreCategoryScene/ViewModel/ExploreCategoryViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ExploreCategoryViewModel.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/07/19.
+//
+
+import Foundation
+
+final class ExploreCategoryViewModel {
+    
+}
