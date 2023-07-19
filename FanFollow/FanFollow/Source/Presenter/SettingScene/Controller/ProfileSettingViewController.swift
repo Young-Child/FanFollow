ce4dc310eb578601f5caa560f3494c6b04e1b556
미니 <cow970814@naver.com>
2023-07-19 15:57:01 +0900

    feat : Profile Setting View Controller 뷰 구성 메서드 추가

--- FanFollow/FanFollow/Source/Presenter/SettingScene/Controller/ProfileSettingViewController.swift
+++ FanFollow/FanFollow/Source/Presenter/SettingScene/Controller/ProfileSettingViewController.swift
@@ -16,15 +16,62 @@ final class ProfileSettingViewController: UIViewController {
         $0.image = UIImage(named: "ExampleProfile")
     }
     
-    private let nickNameLabel = UILabel().then {
-        $0.text = "닉네임"
+    private let nickNameInput = ProfileInputField(title: "닉네임")
+    private let emailInput = ProfileInputField(title: "이메일")
+    
+    private let creatorInformationLabel = UILabel().then {
+        $0.text = "소개 및 상세 정보는 크리에이터만 수정할 수 있습니다."
+        $0.font = .systemFont(ofSize: 13)
+        $0.textColor = UIColor(named: "AccentColor")
     }
+    
+    private let jobCategoryInput = ProfileInputField(title: "분야")
+    private let linkInput = ProfileInputField(title: "링크")
+    private let introduceInput = ProfileInputField(title: "소개")
     
     // Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+    }
+}
+
+extension ProfileSettingViewController {
+    func configureJobCategoryAccessoryView(with pickerView: UIPickerView) -> UIToolbar {
+        let toolbar = UIToolbar()
+        toolbar.barStyle = .default
+        toolbar.isTranslucent = true
+        toolbar.sizeToFit()
+        
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolbar.setItems(
+            [
+                configureCancelButton(),
+                space, configureDoneButton(with: pickerView)
+            ],
+            animated: true
+        )
+        
+        return toolbar
+    }
+    
+    func configureCancelButton() -> UIBarButtonItem {
+        let cancelAction = UIAction { _ in self.jobCategoryInput.endEditing(true) }
+        return UIBarButtonItem(title: "취소", primaryAction: cancelAction)
+    }
+    
+    func configureDoneButton(with pickerView: UIPickerView) -> UIBarButtonItem {
+        let doneAction = UIAction { _ in
+            let row = pickerView.selectedRow(inComponent: 0)
+            let textFieldItem = JobCategory.allCases[row].categoryName
+            
+            self.jobCategoryInput.setText(with: textFieldItem)
+            self.jobCategoryInput.endEditing(true)
+        }
+        
+        return UIBarButtonItem(title: "완료", primaryAction: doneAction)
     }
 }
 
@@ -35,11 +82,27 @@ private extension ProfileSettingViewController {
         navigationController?.setNavigationBarHidden(false, animated: true)
         
         configureHierarchy()
+        configureCategoryPickerView()
         makeConstraints()
     }
     
+    func configureCategoryPickerView() {
+        let pickerView = JobCategoryPickerView()
+        let toolbar = configureJobCategoryAccessoryView(with: pickerView)
+        
+        jobCategoryInput.configureInputView(to: pickerView, with: toolbar)
+    }
+    
     func configureHierarchy() {
-        [profileImageView].forEach(view.addSubview)
+        [
+            profileImageView,
+            nickNameInput,
+            emailInput,
+            creatorInformationLabel,
+            jobCategoryInput,
+            linkInput,
+            introduceInput
+        ].forEach(view.addSubview)
     }
     
     func makeConstraints() {
@@ -48,6 +111,46 @@ private extension ProfileSettingViewController {
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
             $0.width.equalTo(100).priority(.high)
             $0.height.equalTo(100).priority(.high)
+        }
+        
+        nickNameInput.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.top.equalTo(profileImageView.snp.bottom).offset(24)
+            $0.height.equalTo(50)
+        }
+        
+        emailInput.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.top.equalTo(nickNameInput.snp.bottom).offset(24)
+            $0.height.equalTo(50)
+        }
+        
+        creatorInformationLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.top.equalTo(emailInput.snp.bottom).offset(32)
+        }
+        
+        jobCategoryInput.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.top.equalTo(creatorInformationLabel.snp.bottom).offset(16)
+        }
+        
+        linkInput.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.top.equalTo(jobCategoryInput.snp.bottom).offset(32)
+        }
+        
+        introduceInput.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.top.equalTo(linkInput.snp.bottom).offset(32)
         }
     }
 }
