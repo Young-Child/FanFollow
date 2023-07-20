692bc2e4ad1461f3b6d3888bca1492b1e183115d
미니 <cow970814@naver.com>
2023-07-20 17:05:02 +0900

    feat : Update User InformationUseCase 생성

---
+++ FanFollow/FanFollow/Source/Domain/UseCase/UpdateUserInformationUseCase.swift
@@ -0,0 +1,23 @@
+//
+//  UpdateUserInformationUseCase.swift
+//  FanFollow
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import RxSwift
+
+protocol UpdateUserInformationUseCase: AnyObject {
+    func updateUserInformation() -> Observable<Void>
+}
+
+final class DefaultUpdateUserInformationUseCase: UpdateUserInformationUseCase {
+    private let userInformationRepository: UserInformationRepository
+    
+    init(userInformationRepository: UserInformationRepository) {
+        self.userInformationRepository = userInformationRepository
+    }
+    
+    func updateUserInformation() -> Observable<Void> {
+        <#code#>
+    }
+}
