171bf301e470d2d1b415cc5644e120aa226d4a61
미니 <cow970814@naver.com>
2023-07-20 17:06:44 +0900

    feat : Profile Setting View Model 타입 생성

---
+++ FanFollow/FanFollow/Source/Presenter/SettingScene/ViewModel/ProfileSettingViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileSettingViewModel.swift
+//  FanFollow
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import RxSwift
+
+final class ProfileSettingViewModel: ViewModel {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    var disposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
