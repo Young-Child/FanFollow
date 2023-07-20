2725110ee35532a5a9bba2fe6b12df3cf170530f
미니 <cow970814@naver.com>
2023-07-20 17:08:59 +0900

    feat : Profile Setting ViewModel 생성자 구성 및 output 타입 구현

--- FanFollow/FanFollow/Source/Presenter/SettingScene/ViewModel/ProfileSettingViewModel.swift
+++ FanFollow/FanFollow/Source/Presenter/SettingScene/ViewModel/ProfileSettingViewModel.swift
@@ -12,10 +12,25 @@ final class ProfileSettingViewModel: ViewModel {
     }
     
     struct Output {
-        
+        var nickName: Observable<String>
+        var jobCategory: Observable<JobCategory>
+        var links: Observable<[String]>
+        var introduce: Observable<String>
+        var isCreator: Observable<Bool>
     }
     
     var disposeBag = DisposeBag()
+    
+    private let fetchUserInformationUseCase: FetchUserInformationUseCase
+    private let updateUserInformationUseCase: UpdateUserInformationUseCase
+    
+    init(
+        fetchUseCase: FetchUserInformationUseCase,
+        updateUseCase: UpdateUserInformationUseCase
+    ) {
+        self.fetchUserInformationUseCase = fetchUseCase
+        self.updateUserInformationUseCase = updateUseCase
+    }
     
     func transform(input: Input) -> Output {
         return Output()
