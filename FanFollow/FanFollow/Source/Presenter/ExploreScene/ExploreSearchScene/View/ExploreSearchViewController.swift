6a39eacb7d11c463d9b3c33262bbd1b50153bd6f
KyoPak <hyosing92@gmail.com>
2023-07-23 15:09:23 +0900

    config : Explore 탭 내부 SearchViewController, SearchViewModel 파일 생성

---
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/ExploreSearchScene/View/ExploreSearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ExploreSearchViewController.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/07/23.
+//
+
+import UIKit
+
+final class ExploreSearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/ExploreSearchScene/ViewModel/ExploreSearchViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ExploreSearchViewModel.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/07/23.
+//
+
+import Foundation
+
+final class ExploreSearchViewModel {
+    
+}
