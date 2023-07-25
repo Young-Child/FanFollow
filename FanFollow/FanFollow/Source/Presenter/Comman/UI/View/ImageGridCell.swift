5a7619a499c6bf7594e021346e22f79551d66909
미니 <cow970814@naver.com>
2023-07-25 13:41:49 +0900

    docs : Image Grid Cell 주석 추가

--- FanFollow/FanFollow/Source/Presenter/Comman/UI/View/ImageGridCell.swift
+++ FanFollow/FanFollow/Source/Presenter/Comman/UI/View/ImageGridCell.swift
@@ -9,13 +9,16 @@ import UIKit
 import SnapKit
 
 final class ImageGridCell: UICollectionViewCell {
+    // View Properties
     private let imageView = UIImageView().then { imageView in
         imageView.clipsToBounds = true
         imageView.contentMode = .scaleAspectFill
     }
     
+    // Properties
     var identifier: String?
     
+    // Life Cycle
     override func layoutSubviews() {
         super.layoutSubviews()
         
@@ -28,11 +31,13 @@ final class ImageGridCell: UICollectionViewCell {
         self.imageView.image = nil
     }
     
+    // Configure Method
     func setImage(to image: UIImage?) {
         self.imageView.image = image
     }
 }
 
+// Configure UI Method
 private extension ImageGridCell {
     func configureUI() {
         configureHierarchy()
