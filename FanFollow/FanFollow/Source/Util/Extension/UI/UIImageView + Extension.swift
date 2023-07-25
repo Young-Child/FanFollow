265ca43b9af6a2230f48c681dc5214053cd8683f
미니 <cow970814@naver.com>
2023-07-25 23:53:19 +0900

    refactor : UIImage View Extension KF 메서드 내 캐싱 데이터 변경 추가

--- FanFollow/FanFollow/Source/Util/Extension/UI/UIImageView + Extension.swift
+++ FanFollow/FanFollow/Source/Util/Extension/UI/UIImageView + Extension.swift
@@ -16,6 +16,24 @@ extension UIImageView {
     
     func setImageKF(
         to url: String,
+        key: String,
+        failureImage: UIImage?,
+        round: RoundCase = .none
+    ) {
+        self.kf.indicatorType = .activity
+        
+        guard let imageURL = URL(string: url) else {
+            self.image = failureImage
+            return
+        }
+        
+        let resource = ImageResource(downloadURL: imageURL, cacheKey: key)
+        
+        setImage(to: resource, failureImage: failureImage, round: round)
+    }
+    
+    func setImageKF(
+        to url: String,
         failureImage: UIImage,
         round: RoundCase = .none
     ) {
@@ -28,8 +46,12 @@ extension UIImageView {
         
         setImage(to: imageURL, failureImage: failureImage, round: round)
     }
-    private func setImage(to url: URL, failureImage: UIImage, round: RoundCase) {
+    
+    private func setImage(to resource: Resource, failureImage: UIImage?, round: RoundCase) {
+        self.kf.cancelDownloadTask()
+        
         var options = KingfisherOptionsInfo()
+        options.append(.onFailureImage(failureImage))
         
         switch round {
         case .round(let radius):
@@ -39,6 +61,17 @@ extension UIImageView {
             break
         }
         
-        self.kf.setImage(with: url, options: options)
+        self.kf.setImage(with: resource, options: options) { result in
+            switch result {
+            case .success(let image):
+                ImageCache.default.memoryStorage.store(
+                    value: image.image,
+                    forKey: resource.cacheKey
+                )
+                
+            case .failure:
+                break
+            }
+        }
     }
 }
