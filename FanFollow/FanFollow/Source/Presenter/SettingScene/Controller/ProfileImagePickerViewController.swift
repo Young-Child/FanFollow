7529d2cb4aea0b8fededbd2c70b9062e857a711b
미니 <cow970814@naver.com>
2023-07-25 16:05:19 +0900

    feat : Profile Image Picker View Controller 타입 추가

--- FanFollow/FanFollow/Source/Presenter/SettingScene/Controller/ProfileImagePickerViewController.swift
+++ FanFollow/FanFollow/Source/Presenter/SettingScene/Controller/ProfileImagePickerViewController.swift
@@ -3,6 +3,61 @@
 //  FanFollow
 //
 //  Copyright (c) 2023 Minii All rights reserved.
-        
 
-import Foundation
+import UIKit
+
+protocol ProfileImagePickerDelegate: AnyObject {
+    func profileImagePickerViewController(
+        to pickerController: ProfileImagePickerViewController,
+        didSelectedImage image: UIImage?
+    )
+}
+
+final class ProfileImagePickerViewController: PhotoAssetGridViewController {
+    private var maxImageCount: Int
+    private var selectedImage: UIImage? {
+        willSet {
+            delegate?.profileImagePickerViewController(to: self, didSelectedImage: newValue)
+        }
+    }
+    
+    weak var delegate: ProfileImagePickerDelegate?
+    
+    init(maxImageCount: Int = 1) {
+        self.maxImageCount = maxImageCount
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.maxImageCount = 1
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as? AssetImageGridCell
+        cell?.setSelected(to: true)
+        
+        let image = cell?.getImage()
+        self.selectedImage = image
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as? AssetImageGridCell
+        cell?.setSelected(to: false)
+    }
+}
+
+private extension ProfileImagePickerViewController {
+    func configureUI() {
+        let cancelAction = UIAction { _ in
+            self.dismiss(animated: true)
+        }
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "취소", primaryAction: cancelAction)
+    }
+}
