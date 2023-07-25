b888affcc420e79390e0c9b266c4022964b42dcb
junho <junhoL1046@gmail.com>
2023-07-25 23:02:37 +0900

    feat : dataSource 관련 기능 추가 구현

--- FanFollow/FanFollow/Source/Presenter/FeedManageScene/View/FeedManageViewController.swift
+++ FanFollow/FanFollow/Source/Presenter/FeedManageScene/View/FeedManageViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import RxSwift
 import RxRelay
+import RxDataSources
 
 final class FeedManageViewController: UIViewController {
     // View Properties
@@ -15,7 +16,8 @@ final class FeedManageViewController: UIViewController {
         tableView.separatorStyle = .none
         tableView.allowsSelection = false
         tableView.backgroundColor = .systemGray6
-
+        tableView.register(PostCell.self, forCellReuseIdentifier: "PostCell")
+        tableView.register(CreatorInformationCell.self, forCellReuseIdentifier: "CreatorInformationCell")
     }
     private let refreshControl = UIRefreshControl()
 
@@ -24,6 +26,7 @@ final class FeedManageViewController: UIViewController {
     private let viewModel: FeedManageViewModel
     private let likeButtonTap = PublishRelay<String>()
     private let lastCellDisplayed = BehaviorRelay(value: false)
+    private var dataSource: RxTableViewSectionedReloadDataSource<FeedManageSectionModel>!
 
     // Initializer
     init(viewModel: FeedManageViewModel) {
@@ -40,7 +43,33 @@ final class FeedManageViewController: UIViewController {
         super.viewDidLoad()
 
         configureUI()
+        configureDataSource()
         binding()
+    }
+
+    private func configureDataSource() {
+        dataSource = RxTableViewSectionedReloadDataSource<FeedManageSectionModel>(
+            configureCell: { dataSource, tableView, indexPath, item in
+                switch dataSource[indexPath.section] {
+                case .creatorInformation(let items):
+                    guard let cell = tableView.dequeueReusableCell(
+                        withIdentifier: "CreatorInformationCell", for: indexPath
+                    ) as? CreatorInformationCell else { return UITableViewCell() }
+                    let item = items[indexPath.row]
+                    let creator = item.creator
+                    let followerCount = item.followerCount
+                    cell.configure(with: creator, followerCount: followerCount)
+                    return cell
+                case .posts(let items):
+                    guard let cell = tableView.dequeueReusableCell(
+                        withIdentifier: "PostCell", for: indexPath
+                    ) as? PostCell else { return UITableViewCell() }
+                    let item = items[indexPath.row]
+                    cell.configure(with: item, delegate: self, creatorViewIsHidden: true)
+                    return cell
+                }
+            }
+        )
     }
 }
 
@@ -53,16 +82,36 @@ private extension FeedManageViewController {
     }
 
     func input() -> FeedManageViewModel.Input {
+        let lastCellDisplayed = tableView.rx.didScroll
+            .withUnretained(self)
+            .flatMap({ _, _ -> Observable<Void> in
+                guard self.lastCellDisplayed.value == false else { return .empty() }
+                let offsetY = self.tableView.contentOffset.y
+                let contentHeight = self.tableView.contentSize.height
+                let frameHeight = self.tableView.frame.size.height
+                let lastCellDisplayed = offsetY > (contentHeight - frameHeight)
+                self.lastCellDisplayed.accept(lastCellDisplayed)
+                return lastCellDisplayed ? .just(()) : .empty()
+            })
+
         return FeedManageViewModel.Input(
-            viewWillAppear: .empty(),
-            refresh: .empty(),
-            lastCellDisplayed: .empty(),
-            likeButtonTap: .empty()
+            viewWillAppear: rx.methodInvoked(#selector(viewWillAppear)).map { _ in }.asObservable(),
+            refresh: refreshControl.rx.controlEvent(.valueChanged).asObservable(),
+            lastCellDisplayed: lastCellDisplayed,
+            likeButtonTap: likeButtonTap.asObservable()
         )
     }
 
     func bindTableView(_ output: FeedManageViewModel.Output) {
-
+        output.feedManageSections
+            .asDriver(onErrorJustReturn: [])
+            .do { [weak self] _ in
+                guard let self else { return }
+                self.refreshControl.endRefreshing()
+                self.lastCellDisplayed.accept(false)
+            }
+            .drive(tableView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
     }
 }
 
@@ -86,7 +135,6 @@ private extension FeedManageViewController {
     }
 
     func configureNavigationItem() {
-        let image = UIImage.add
         let barButtonItem = UIBarButtonItem(
             systemItem: .add,
             primaryAction: UIAction(handler: { _ in
@@ -97,6 +145,18 @@ private extension FeedManageViewController {
     }
 
     func configureTableView() {
+        tableView.backgroundColor = .systemBackground
         tableView.refreshControl = refreshControl
+    }
+}
+
+// PostCellDelegate
+extension FeedManageViewController: PostCellDelegate {
+    func performTableViewBathUpdates(_ updates: (() -> Void)?) {
+        tableView.performBatchUpdates(updates)
+    }
+
+    func likeButtonTap(postID: String) {
+        likeButtonTap.accept(postID)
     }
 }
