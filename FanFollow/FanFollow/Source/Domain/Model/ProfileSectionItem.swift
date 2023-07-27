eb761bd5c5099f8a96d025df56d69f1d2b35c92d
junho <junhoL1046@gmail.com>
2023-07-27 21:24:05 +0900

    feat : ProfileSectionItem 구현

---
+++ FanFollow/FanFollow/Source/Domain/Model/ProfileSectionItem.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileSectionItem.swift
+//  FanFollow
+//
+//  Created by junho lee on 2023/07/27.
+//
+
+struct ProfileSectionItem {
+    var creator: Creator
+    var followerCount: Int
+    var isFollow: Bool
+}
