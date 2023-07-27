4917884a7f433534483bc5ff25de21882d068e84
junho <junhoL1046@gmail.com>
2023-07-27 10:09:28 +0900

    feat : FeedCoordinator 구현

---
+++ FanFollow/FanFollow/Source/Coordinator/FeedCoordinator.swift
@@ -0,0 +1,49 @@
+//
+//  FeedCoordinator.swift
+//  FanFollow
+//
+//  Created by junho lee on 2023/07/27.
+//
+
+import UIKit
+
+final class FeedCoordinator: Coordinator {
+    weak var parentCoordinator: MainTabBarCoordinator?
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        // TODO: 로그인한 UserID를 followerID에 입력
+        let userID = "5b587434-438c-49d8-ae3c-88bb27a891d4"
+        presentFeedViewController(userID: userID)
+    }
+
+    func presentFeedViewController(userID: String) {
+        let fetchFeedUseCase = DefaultFetchFeedUseCase(
+            postRepository: DefaultPostRepository(networkService: DefaultNetworkService())
+        )
+        let changeLikeUseCase = DefaultChangeLikeUseCase(
+            likeRepository: DefaultLikeRepository(networkService: DefaultNetworkService())
+        )
+
+        let feedViewModel = FeedViewModel(
+            fetchFeedUseCase: fetchFeedUseCase,
+            changeLikeUseCase: changeLikeUseCase,
+            followerID: userID
+        )
+        let feedViewController = FeedViewController(viewModel: feedViewModel)
+
+        navigationController.pushViewController(feedViewController, animated: true)
+    }
+
+    func presentProfileViewController(creatorID: String) {
+        // TODO: ProfileViewController 생성 후 수정
+        let profileViewController = UIViewController()
+
+        navigationController.pushViewController(profileViewController, animated: true)
+    }
+}
