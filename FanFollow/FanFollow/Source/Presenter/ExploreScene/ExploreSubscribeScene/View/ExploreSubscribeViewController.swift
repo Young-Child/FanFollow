128c3e3e16f37ac18a06517c684f4aa0c156d995
KyoPak <hyosing92@gmail.com>
2023-07-28 14:29:09 +0900

    feat : ExploreSubscribeViewController 기본 구현

--- FanFollow/FanFollow/Source/Presenter/ExploreScene/ExploreSubscribeScene/View/ExploreSubscribeViewController.swift
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/ExploreSubscribeScene/View/ExploreSubscribeViewController.swift
@@ -6,10 +6,55 @@
 //
 
 import UIKit
+import RxSwift
 
-class ExploreSubscribeViewController: UIViewController {
+final class ExploreSubscribeViewController: UIViewController {
+    // View Property
+    private let subscribeTableView = UITableView().then {
+        $0.separatorColor = .lightGray
+        $0.backgroundColor = .clear
+        $0.register(CreatorListCell.self, forCellReuseIdentifier: CreatorListCell.reuseIdentifier)
+    }
     
+    // Properties
+    weak var coordinator: ExploreCoordinator?
+    private let viewModel: ExploreSubscribeViewModel
+    private let disposeBag = DisposeBag()
+    
+    // Initializer
+    init(viewModel: ExploreSubscribeViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureUI()
+    }
+}
+
+// Configure UI
+private extension ExploreSubscribeViewController {
+    func configureUI() {
+        view.backgroundColor = .systemBackground
+        
+        configureHierarchy()
+        makeConstraints()
+    }
+    
+    func configureHierarchy() {
+        view.addSubview(subscribeTableView)
+    }
+    
+    func makeConstraints() {
+        subscribeTableView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
     }
 }
