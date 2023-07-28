c23b20b40d28f1ca520adbbf791b6b57eb98f89c
KyoPak <hyosing92@gmail.com>
2023-07-28 14:21:40 +0900

    config : ExploreSubscribeView, ViewModel 파일 및 폴더 추가

---
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/ExploreSubscribeScene/View/ExploreSubscribeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ExploreSubscribeViewController.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/07/28.
+//
+
+import UIKit
+
+class ExploreSubscribeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/ExploreSubscribeScene/ViewModel/ExploreSubscribeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ExploreSubscribeViewModel.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/07/28.
+//
+
+import Foundation
+
+final class ExploreSubscribeViewModel {
+    
+}
