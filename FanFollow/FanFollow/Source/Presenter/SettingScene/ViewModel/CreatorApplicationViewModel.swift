7846c97d30c2f3016bbda9f2117fba9cbd9d793e
junho <junhoL1046@gmail.com>
2023-08-01 18:51:18 +0900

    refactor : Input, Output 프로퍼티 수정 및 creatorInformation Relay 제거

--- FanFollow/FanFollow/Source/Presenter/SettingScene/ViewModel/CreatorApplicationViewModel.swift
+++ FanFollow/FanFollow/Source/Presenter/SettingScene/ViewModel/CreatorApplicationViewModel.swift
@@ -11,46 +11,41 @@ import RxRelay
 final class CreatorApplicationViewModel: ViewModel {
     struct Input {
         var viewWillAppear: Observable<Void>
-        var categoryChanged: Observable<JobCategory>
-        var linksChanged: Observable<[String]>
-        var introduceChanged: Observable<String>
         var backButtonTap: Observable<Void>
-        var nextButtonTap: Observable<Void>
+        var nextButtonTap: Observable<CreatorInformation>
     }
 
     struct Output {
         var creatorApplicationStep: Observable<CreatorApplicationStep>
-        var creatorInformation: Observable<CreatorInformation>
     }
 
-    typealias CreatorInformation = (category: JobCategory?, links: [String]?, introduce: String?)
+    typealias CreatorInformation = (category: Int?, links: [String]?, introduce: String?)
 
     var disposeBag = DisposeBag()
-    private let creatorApplicationUseCase: ApplyCreatorUseCase
+    private let applyCreatorUseCase: ApplyCreatorUseCase
     private let userID: String
-    private let creatorInformation = BehaviorRelay<CreatorInformation>(value:(nil, nil, nil))
     private let creatorApplicationStep = BehaviorRelay(value: CreatorApplicationStep.category)
 
-    init(creatorApplicationUseCase: ApplyCreatorUseCase, userID: String) {
-        self.creatorApplicationUseCase = creatorApplicationUseCase
+    init(applyCreatorUseCase: ApplyCreatorUseCase, userID: String) {
+        self.applyCreatorUseCase = applyCreatorUseCase
         self.userID = userID
     }
 
     func transform(input: Input) -> Output {
         let initialStep = input.viewWillAppear
             .withUnretained(self)
-            .flatMapLatest { _ -> Observable<CreatorApplicationStep> in
-                return self.creatorApplicationStep.asObservable()
+            .flatMapFirst { _ -> Observable<CreatorApplicationStep> in
+                return .just(self.creatorApplicationStep.value)
             }
 
         let nextStep = input.nextButtonTap
             .withUnretained(self)
-            .flatMapLatest { _ -> Observable<CreatorApplicationStep> in
+            .flatMapFirst { _, creatorInformation -> Observable<CreatorApplicationStep> in
                 let currentStep = self.creatorApplicationStep.value
                 let nextStep = currentStep.next
                 switch currentStep {
                 case .introduce:
-                    return self.registerCreatorInformation().andThen(Observable.just(nextStep))
+                    return self.registerCreatorInformation(creatorInformation).andThen(Observable.just(nextStep))
                 default:
                     return .just(nextStep)
                 }
@@ -58,7 +53,7 @@ final class CreatorApplicationViewModel: ViewModel {
 
         let previousStep = input.backButtonTap
             .withUnretained(self)
-            .flatMapLatest { _ -> Observable<CreatorApplicationStep> in
+            .flatMapFirst { _ -> Observable<CreatorApplicationStep> in
                 let currentStep = self.creatorApplicationStep.value
                 let previousStep = currentStep.previous
                 return .just(previousStep)
@@ -69,56 +64,20 @@ final class CreatorApplicationViewModel: ViewModel {
                 self.creatorApplicationStep.accept(step)
             }
 
-        let initialCreatorInformation = input.viewWillAppear
-            .withUnretained(self)
-            .flatMapLatest { _ -> Observable<CreatorInformation> in
-                return .just((category: .art, links: [], introduce: nil))
-            }
-
-        let updatedCreatorInformation = Observable.merge(
-            input.categoryChanged.map { category -> CreatorInformation in (category, nil, nil) },
-            input.linksChanged.map { links -> CreatorInformation in (nil, links, nil) },
-            input.introduceChanged.map { introduce -> CreatorInformation in (nil, nil, introduce) }
-        )
-            .withUnretained(self)
-            .flatMapLatest { _, updatedCreatorInformation in
-                return self.updatedCreatorInformation(updatedCreatorInformation)
-            }
-
-        let creatorInformation = Observable.merge(initialCreatorInformation, updatedCreatorInformation)
-            .do { creatorInformation in
-                self.creatorInformation.accept(creatorInformation)
-            }
-
-        return Output(creatorApplicationStep: creatorApplicationStep, creatorInformation: creatorInformation)
+        return Output(creatorApplicationStep: creatorApplicationStep)
     }
 }
 
 private extension CreatorApplicationViewModel {
-    func registerCreatorInformation() -> Completable {
-        let creatorInformation = self.creatorInformation.value
-        return self.creatorApplicationUseCase.applyCreator(
+    func registerCreatorInformation(_ creatorInformation: CreatorInformation) -> Completable {
+        return self.applyCreatorUseCase.applyCreator(
             userID: self.userID,
             creatorInformation: (
-                creatorInformation.category?.rawValue,
+                creatorInformation.category,
                 creatorInformation.links,
                 creatorInformation.introduce
             )
         )
-    }
-
-    func updatedCreatorInformation(_ creatorInformation: CreatorInformation) -> Observable<CreatorInformation> {
-        var creatorInformation = self.creatorInformation.value
-        if let category = creatorInformation.category {
-            creatorInformation.category = category
-        }
-        if let links = creatorInformation.links {
-            creatorInformation.links = links
-        }
-        if let introduce = creatorInformation.introduce {
-            creatorInformation.introduce = introduce
-        }
-        return .just(creatorInformation)
     }
 }
 
