4dd28e6a5bd6aaadc75f860a523118d3160bd7c7
KyoPak <hyosing92@gmail.com>
2023-08-01 15:37:04 +0900

    config : PostViewController, PostViewModel 파일 생성

---
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/PostScene/View/PostViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PostViewController.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/08/01.
+//
+
+import UIKit
+
+final class PostViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/PostScene/ViewModel/PostViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  PostViewModel.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/08/01.
+//
+
+import Foundation
+
+final class PostViewModel {
+    
+}
