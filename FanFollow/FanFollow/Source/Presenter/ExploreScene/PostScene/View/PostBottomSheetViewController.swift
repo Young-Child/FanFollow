13f501a0aa66f40816a129d9485d4ce5a95231ba
KyoPak <hyosing92@gmail.com>
2023-08-01 10:11:54 +0900

    config : PostBottomSheetView 파일 생성 및 PostScene 폴더 생성

---
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/PostScene/View/PostBottomSheetViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PostBottomSheetViewController.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/08/01.
+//
+
+import UIKit
+
+final class PostBottomSheetViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
