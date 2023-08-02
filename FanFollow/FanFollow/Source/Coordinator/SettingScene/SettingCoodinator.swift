ab478e8f78641096c0e32fdb5dd1a227a2aea102
junho <junhoL1046@gmail.com>
2023-08-02 14:07:51 +0900

    feat : generateCoordinator 메서드 .creator 케이스 coordinator 구현

--- FanFollow/FanFollow/Source/Coordinator/SettingScene/SettingCoodinator.swift
+++ FanFollow/FanFollow/Source/Coordinator/SettingScene/SettingCoodinator.swift
@@ -41,6 +41,8 @@ private extension SettingCoordinator {
         switch viewType {
         case .profile:
             return ProfileSettingCoordinator(navigationController: navigationController)
+        case .creator:
+            return CreatorApplicationCoordinator(navigationController: navigationController)
         default:
             return ProfileSettingCoordinator(navigationController: navigationController)
         }
