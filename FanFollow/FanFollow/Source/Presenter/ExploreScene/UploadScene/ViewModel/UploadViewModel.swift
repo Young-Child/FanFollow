09b1d966583ddf965d2910dd2db01d9c088caa2a
KyoPak <hyosing92@gmail.com>
2023-08-02 17:36:19 +0900

    feat : UploadViewModel 구현

--- FanFollow/FanFollow/Source/Presenter/ExploreScene/UploadScene/ViewModel/UploadViewModel.swift
+++ FanFollow/FanFollow/Source/Presenter/ExploreScene/UploadScene/ViewModel/UploadViewModel.swift
@@ -5,8 +5,36 @@
 //  Created by parkhyo on 2023/08/01.
 //
 
-import Foundation
+import RxSwift
 
-final class UploadViewModel {
+final class UploadViewModel: ViewModel {
+    struct Input {
+        var registerButtonTap: Observable<Post>
+    }
     
+    struct Output {
+        var registerResult: Observable<Void>
+    }
+    
+    var disposeBag = DisposeBag()
+    private let uploadUseCase: UploadPostUseCase
+    
+    init(uploadUseCase: UploadPostUseCase) {
+        self.uploadUseCase = uploadUseCase
+    }
+    
+    func transform(input: Input) -> Output {
+        let registerResult = input.registerButtonTap
+            .flatMapLatest { uploadData in
+                return self.uploadUseCase
+                    .upsertPost(
+                        userID: uploadData.userID, title: uploadData.title,
+                        content: uploadData.content, imageURLs: uploadData.imageURLs ?? [],
+                        videoURL: uploadData.videoURL
+                    )
+                    .andThen(Observable.just(()))
+            }
+        
+        return Output(registerResult: registerResult)
+    }
 }
