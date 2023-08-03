1490ee3b3a9b54834496661a1c8a4315e9af8d2e
KyoPak <hyosing92@gmail.com>
2023-08-03 11:43:33 +0900

    feat : Upload 타입 추가

---
+++ FanFollow/FanFollow/Source/Domain/Model/Upload.swift
@@ -0,0 +1,15 @@
+//
+//  Upload.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/08/03.
+//
+
+import UIKit
+
+struct Upload {
+    let title: String?
+    let content: String
+    let imageDatas: [Data]
+    let videoURL: String?
+}
