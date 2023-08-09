291aa0aa8d6fd642d02220b847985df396ad3463
미니 <cow970814@naver.com>
2023-08-09 22:22:16 +0900

    refactor : Creator Introduce View Controller View 설정

--- FanFollow/FanFollow/Source/Presenter/SettingScene/Controller/CreatorApplication/ChildCreatorApplicationControllers/CreatorIntroduceViewController.swift
+++ FanFollow/FanFollow/Source/Presenter/SettingScene/Controller/CreatorApplication/ChildCreatorApplicationControllers/CreatorIntroduceViewController.swift
@@ -27,7 +27,8 @@ final class CreatorIntroduceViewController: CreatorApplicationChildController {
         
         view.addSubview(introduceTextView)
         introduceTextView.snp.makeConstraints {
-            $0.top.leading.trailing.bottom.equalToSuperview()
+            $0.top.leading.trailing.equalToSuperview().inset(8)
+            $0.bottom.equalToSuperview()
         }
         
         bind()
