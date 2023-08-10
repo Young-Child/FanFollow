8dc3bbded79a4ce99c250f29f8e3da8bbc588589
KyoPak <hyosing92@gmail.com>
2023-08-10 16:54:27 +0900

    config : AppleSigningUseCase 파일 생성 및 LogInScene 관련 그룹 생성

---
+++ FanFollow/FanFollow/Source/Domain/UseCase/LogInScene/AppleSigningUseCase.swift
@@ -0,0 +1,16 @@
+//
+//  AppleSigningUseCase.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/08/10.
+//
+
+import Foundation
+
+protocol AppleSigningUseCase {
+    
+}
+
+final class DefaultAppleSigningUseCase {
+    
+}
