5106b6ece918f5e5c2d16b34405c46ab0c411251
KyoPak <hyosing92@gmail.com>
2023-08-11 15:26:48 +0900

    feat : AppleSigningUseCase 내부 SignIn 메서드 구현

--- FanFollow/FanFollow/Source/Domain/UseCase/LogInScene/AppleSigningUseCase.swift
+++ FanFollow/FanFollow/Source/Domain/UseCase/LogInScene/AppleSigningUseCase.swift
@@ -5,12 +5,45 @@
 //  Created by parkhyo on 2023/08/10.
 //
 
-import Foundation
+import RxSwift
 
 protocol AppleSigningUseCase {
-    
+    func signIn(with idToken: String) -> Completable
 }
 
 final class DefaultAppleSigningUseCase {
+    private let authRepository: AuthRepository
+    private let disposeBag = DisposeBag()
     
+    init(authRepository: AuthRepository) {
+        self.authRepository = authRepository
+    }
+    
+    func signIn(with idToken: String) -> Completable {
+        return Completable.create { completable in
+            let disposable = self.authRepository.storedSession()
+                .subscribe(onNext: { storedSession in
+                    completable(.completed)
+                }, onError: { error in
+                    let signInDisposable = self.authRepository.signIn(with: idToken, of: .apple)
+                        .subscribe { _ in
+                            completable(.completed)
+                        } onError: { error in
+                                completable(.error(error))
+                        }
+                    
+                    return
+                })
+            
+            return Disposables.create {
+                disposable.dispose()
+            }
+        }
+    }
+}
+
+private extension DefaultAppleSigningUseCase {
+    enum Constants {
+        static let session = "session"
+    }
 }
