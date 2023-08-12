34b0d2e6002fb281084e77cac2f248a651e82449
KyoPak <hyosing92@gmail.com>
2023-08-12 14:50:03 +0900

    config : AppleSigningUseCaseTests 파일 생성

---
+++ FanFollow/UseCaseTests/AppleSigningUseCaseTests.swift
@@ -0,0 +1,18 @@
+//
+//  AppleSigningUseCaseTests.swift
+//  UseCaseTests
+//
+//  Created by parkhyo on 2023/08/12.
+//
+
+import XCTest
+
+import RxSwift
+import RxTest
+import RxBlocking
+
+@testable import FanFollow
+
+final class AppleSigningUseCaseTests: XCTestCase {
+    
+}
