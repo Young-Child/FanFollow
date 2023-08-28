c60db51ff8cd35d044dfba8b6e36eadf980a971d
KyoPak <hyosing92@gmail.com>
2023-08-28 15:37:58 +0900

    config : CreatorAgreementViewController 파일 생성

---
+++ FanFollow/FanFollow/Source/Presenter/SettingScene/CreatorApplicationView/Controllers/ChildControllers/CreatorAgreementViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreatorAgreementViewController.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/08/28.
+//
+
+import UIKit
+
+final class CreatorAgreementViewController: CreatorApplicationChildController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
