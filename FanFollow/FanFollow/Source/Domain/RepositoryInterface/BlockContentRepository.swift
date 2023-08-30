a4f55247199a9a2e46094a0e1dbccc05c677cd11
KyoPak <hyosing92@gmail.com>
2023-08-30 16:49:54 +0900

    feat : BlockContentRepository 프로토콜 파일 생성 및 메서드 정의

---
+++ FanFollow/FanFollow/Source/Domain/RepositoryInterface/BlockContentRepository.swift
@@ -0,0 +1,13 @@
+//
+//  BlockContentRepository.swift
+//  FanFollow
+//
+//  Created by parkhyo on 2023/08/30.
+//
+
+import Foundation
+import RxSwift
+
+protocol BlockContentRepository: SupabaseEndPoint {
+    func blockPost(_ postID: String, to userID: UUID) -> Completable
+}
