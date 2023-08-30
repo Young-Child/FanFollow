b3e6b331632e48184d879500b97cdc31f3af6eda
미니 <cow970814@naver.com>
2023-08-30 11:12:57 +0900

    refactor : Creator Agreement View Controller 안내 사항으로 변경 및 체크박스 제거

--- FanFollow/FanFollow/Source/Presenter/SettingScene/CreatorApplicationView/Controllers/ChildControllers/CreatorAgreementViewController.swift
+++ FanFollow/FanFollow/Source/Presenter/SettingScene/CreatorApplicationView/Controllers/ChildControllers/CreatorAgreementViewController.swift
@@ -17,44 +17,11 @@ final class CreatorAgreementViewController: CreatorApplicationChildController {
         $0.font = .coreDreamFont(ofSize: 14, weight: .regular)
     }
     
-    private let agreeCheckButton = UIButton().then {
-        $0.tintColor = Constants.Color.blue
-        $0.setImage(Constants.Image.checkMark, for: .selected)
-        $0.setImage(Constants.Image.square, for: .normal)
-    }
-    
-    private let agreeLabel = UILabel().then {
-        $0.numberOfLines = 0
-        $0.textColor = Constants.Color.gray
-        $0.text = Constants.Text.creatorAgreement
-        $0.font = .coreDreamFont(ofSize: 17, weight: .regular)
-    }
-    
-    private let agreeStackView = UIStackView().then {
-        $0.spacing = 8
-        $0.alignment = .fill
-        $0.axis = .horizontal
-        $0.distribution = .fill
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
-        bind()
-    }
-    
-    private func bind() {
-        agreeCheckButton.rx.tap
-            .map {
-                return !self.agreeCheckButton.isSelected
-            }
-            .do {
-                self.agreeCheckButton.isSelected = !self.agreeCheckButton.isSelected
-                self.agreeLabel.textColor = $0 ? Constants.Color.label : Constants.Color.gray
-            }
-            .bind(to: nextButtonEnable)
-            .disposed(by: disposeBag)
+        nextButtonEnable.accept(true)
     }
 }
 
@@ -65,19 +32,14 @@ private extension CreatorAgreementViewController {
     }
     
     func configureHierarchy() {
-        [agreeCheckButton, agreeLabel].forEach(agreeStackView.addArrangedSubview(_:))
-        [agreeTextView, agreeStackView].forEach(view.addSubview(_:))
+        view.addSubview(agreeTextView)
     }
     
     func makeConstraints() {
         agreeTextView.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview().inset(Constants.Spacing.small)
-            $0.height.equalToSuperview().multipliedBy(0.8)
-        }
-        
-        agreeStackView.snp.makeConstraints {
-            $0.top.equalTo(agreeTextView.snp.bottom).offset(Constants.Spacing.medium)
-            $0.centerX.equalToSuperview()
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalToSuperview().offset(Constants.Spacing.medium)
+            $0.trailing.equalToSuperview().offset(-Constants.Spacing.medium)
         }
     }
 }
