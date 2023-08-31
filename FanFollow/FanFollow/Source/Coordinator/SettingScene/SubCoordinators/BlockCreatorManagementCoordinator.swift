231ea68a2168744fa2421c450b045bce71059dcb
junho <junhoL1046@gmail.com>
2023-08-31 18:07:06 +0900

    feat : BlockCreatorManagementCoordinator 구현

---
+++ FanFollow/FanFollow/Source/Coordinator/SettingScene/SubCoordinators/BlockCreatorManagementCoordinator.swift
@@ -0,0 +1,49 @@
+//
+//  BlockCreatorManagementCoordinator.swift
+//  FanFollow
+//
+//  Created by junho lee on 2023/08/31.
+//
+
+import UIKit
+
+final class BlockCreatorManagementCoordinator: Coordinator {
+    weak var parentCoordinator: Coordinator?
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let networkService = DefaultNetworkService.shared
+        let userDefaultsService = UserDefaults.standard
+        let blockUserRepository = DefaultBlockUserRepository(networkService: networkService)
+        let userInformationRepository = DefaultUserInformationRepository(networkService)
+        let authRepository = DefaultAuthRepository(
+            networkService: networkService,
+            userDefaultsService: userDefaultsService
+        )
+        let manageBlockCreatorUseCase = DefaultManageBlockCreatorUseCase(
+            blockCreatorUseCase: blockUserRepository,
+            userInformationRepository: userInformationRepository,
+            authRepository: authRepository
+        )
+
+        let blockCreatorManagementViewModel = BlockCreatorManagementViewModel(
+            manageBlockCreatorUseCase: manageBlockCreatorUseCase
+        )
+
+        let blockCreatorManagementViewController = BlockCreatorManagementViewController(
+            viewModel: blockCreatorManagementViewModel
+        )
+        blockCreatorManagementViewController.coordinator = self
+        blockCreatorManagementViewController.hidesBottomBarWhenPushed = true
+
+        navigationController.pushViewController(
+            blockCreatorManagementViewController,
+            animated: true
+        )
+    }
+}
