673d52e8f97e6ad32633d0c1ac7ff567a97aa7d8
KyoPak <hyosing92@gmail.com>
2023-08-31 16:10:06 +0900

    refactor : ReportType, ReportReason 프로퍼티 명 수정

--- FanFollow/FanFollow/Source/Domain/Model/Feed/ReportType.swift
+++ FanFollow/FanFollow/Source/Domain/Model/Feed/ReportType.swift
@@ -10,21 +10,30 @@ import Foundation
 enum ReportType {
     case content
     case user
+    
+    var title: String {
+        switch self {
+        case .content:
+            return "게시물을 "
+        case .user:
+            return "사용자를 "
+        }
+    }
 }
 
 struct ReportReason {
-    var title: String
+    var reason: String
 }
 
 extension ReportReason {
     private static var commonReasons: [Self] {
         return [
-            .init(title: "스팸"),
-            .init(title: "나체 이미지 또는 성적 행위"),
-            .init(title: "사기 또는 거짓"),
-            .init(title: "혐오 발언 또는 상징"),
-            .init(title: "거짓 정보"),
-            .init(title: "저작권 침해")
+            .init(reason: "스팸"),
+            .init(reason: "나체 이미지 또는 성적 행위"),
+            .init(reason: "사기 또는 거짓"),
+            .init(reason: "혐오 발언 또는 상징"),
+            .init(reason: "거짓 정보"),
+            .init(reason: "저작권 침해")
         ]
     }
     
@@ -33,9 +42,9 @@ extension ReportReason {
         
         switch reportType {
         case .content:
-            commonReason.insert(.init(title: "마음에 들지 않습니다. (숨김 처리)"), at: .zero)
+            commonReason.insert(.init(reason: "마음에 들지 않습니다. (숨김 처리)"), at: .zero)
         case .user:
-            commonReason.insert(.init(title: "사칭"), at: .zero)
+            commonReason.insert(.init(reason: "사칭"), at: .zero)
         }
         
         return commonReason
