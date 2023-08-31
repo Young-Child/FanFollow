235e3480386e9b1cd45b6fa061dc30d0e6de2f52
미니 <cow970814@naver.com>
2023-08-31 17:57:52 +0900

    feat : Block User ViewModel 타입 구현

---
+++ FanFollow/FanFollow/Source/Presenter/SettingScene/ProfileFeedView/ViewModel/BlockUserViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  BlockUserViewModel.swift
+//  FanFollow
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import RxSwift
+
+class BlockUserViewModel: ViewModel {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    var disposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
